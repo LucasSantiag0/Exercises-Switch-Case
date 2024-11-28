24489e3cb854337caaf1b02fce9860d6296ba5bb
LucasSantiag0 <lucas.sank@hotmail.com>
2024-11-28 09:31:04 -0300

    Exercises

---
+++ Exercicicos_Switch_case/Program.cs
@@ -0,0 +1,12 @@
+﻿using Exercicios_Switch_Case;
+
+//Exercicio1.Main(args);
+//Exercicio2.Main(args);
+//Exercicio3.Main(args);
+//Exercicio4.Main(args);
+//Exercicio5.Main(args);
+//Exercicio6.Main(args);
+//Exercicio7.Main(args);
+//Exercicio8.Main(args);
+//Exercicio9.Main(args);
+Exercicio10.Main(args);
